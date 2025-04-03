aec3e343d3166903ec0faf426fb09c1a26a40070
A.Naciri <abdelhaqnaciri0@gmail.com>
2025-04-03 02:43:48 +0000

    Initialized hello file

---
+++ hello.c
@@ -0,0 +1,13 @@
+/*
+* Basic C Program
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv){
+    printf("Hello Word!\n");
+
+    printf("Hello C!\n");
+
+    return 0;
+}
